c044e322e7a825b1f4d7c37b3d23c68d74d94b35
Orkhan Talibli <orkhan.talibli@gmail.com>
2023-12-26 23:52:49 +0400

    Profile page functionality

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Offers from "./pages/Offers";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import NotFound from "./pages/NotFound";
-import Profile from './pages/Profile.jsx';
+import Profile from "./pages/Profile.jsx";
 import ForgotPassword from "./pages/ForgotPassword";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -20,7 +21,9 @@ function App() {
           <Route path="/offers" element={<Offers />} />
           <Route path="/signIn" element={<SignIn />} />
           <Route path="/signUp" element={<SignUp />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="*" element={<NotFound />} />
           <Route path="/forgotPassword" element={<ForgotPassword />} />
         </Routes>
